0f494f8cc58ee9f1b85ed4568162474a700f1609
Stanislav Burdukowsky <burdukowskystas@gmail.com>
2019-11-17 19:28:36 +0700

    add switchTheme button

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,10 +1,13 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+import {DOCUMENT} from '@angular/common';
 
 import {Command} from '../command.enum';
 import {ControlService} from '../services/control.service';
 import {LocalStorageService} from '../services/local-storage.service';
 import {Layout} from '../layout.enum';
+
+export type AppTheme = 'default-theme' | 'dark-theme';
 
 @Component({
   selector: 'app-main',
@@ -15,12 +18,14 @@ export class MainComponent implements OnInit {
 
   command = Command;
   layout = Layout;
+  appTheme: AppTheme = 'dark-theme';
 
   currentLayout = Layout.Stretch;
 
   constructor(private controlService: ControlService,
               private localStorageService: LocalStorageService,
-              private router: Router) {
+              private router: Router,
+              @Inject(DOCUMENT) private document: Document) {
   }
 
   ngOnInit() {
@@ -35,6 +40,11 @@ export class MainComponent implements OnInit {
 
   switchLayout() {
     this.currentLayout = this.currentLayout === Layout.Classic ? Layout.Stretch : Layout.Classic;
+  }
+
+  switchTheme() {
+    this.appTheme = this.appTheme === 'default-theme' ? 'dark-theme' : 'default-theme';
+    this.document.body.className = this.appTheme;
   }
 
 }
